54491b16207e4de98d645fb8ebb8f27a06c305f9
Kaminoatowo <j.cocomello@gmail.com>
2024-02-17 19:25:53 +0100

    start dev

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,7 +1,7 @@
 <footer class="bg-component">
     <hr class="border-1 border-black w-full mx-auto">
     <div class="bg-secondary my-2 mx-5 text-center">
-        <p class="m-0 ml-auto mr-auto text-center">© 2024 OGD design 🦡
+        <p class="m-0 ml-auto mr-auto text-center">© 2024 OGD design 🦡 DEV
             <br>
             All rights reserved.
         </p>
