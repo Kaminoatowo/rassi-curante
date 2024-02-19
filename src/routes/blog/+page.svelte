5795af1d4c8efd3120a902e8fb53a063d8ff57a5
Kaminoatowo <j.cocomello@gmail.com>
2024-02-19 19:06:53 +0100

    create post write branch

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -3,34 +3,29 @@
     const uniqueCategories = [...new Set(data.posts.map(post => post.meta.categories).flat())];
 </script>
 
-<head>
-    <title>Blog</title>
-</head>
-
 <h1>Blog</h1>
 
-
-<div class="columns-3 m-2 justify-center">
+<ul>
     {#each data.posts as post}
-            <span class="flex flex-col">
-                <div class="bg-secondary/80 my-2 flex flex-col">
-                    <a href={post.path} class="m-3 flex justify-center">
-                        <img src={post.meta.picture} alt={post.meta.title} class="w-48 h-32 rounded-xl border-2 border-solid border-ternary" />
+        <li>
+            <h2>
+                <span class="flex flex-row">
+                    <a href={post.path} class="w-1/5 m-3">
+                        <img src={post.meta.picture} alt={post.meta.title} class="rounded-xl border-4 border-solid border-ternary" />
                     </a>
-                    <span class="mx-auto">
+                    <span>
                         <a href="{post.path}">
                             {post.meta.title}
                         </a>
-                        <br/>
+                        <br />
                         Published on {post.meta.date}
                     </span>
-                </div>
-            </span>
+                </span>
+            </h2>
+            
+        </li>
     {/each}
-</div>
-
-
-<hr class="border-1 border-black w-full mx-auto">
+</ul>
 
 <h2>Categories</h2>
 
@@ -43,3 +38,5 @@
         </li>
     {/each}
 </ul>
+
+<a href="/admin/write" class="btn">New Post</a>
