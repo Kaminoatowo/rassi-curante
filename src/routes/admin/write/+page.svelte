f3a6b2f58fc750abdc6ae2d2afc2721e778111f6
Kaminoatowo <j.cocomello@gmail.com>
2024-02-22 18:52:13 +0100

    minor edit

--- src/routes/admin/write/+page.svelte
+++ src/routes/admin/write/+page.svelte
@@ -16,6 +16,7 @@
 <h1 class="flex justify-center m-2">Write a new post</h1>
 
 <form on:submit={handleSubmit} class="m-3 p-2 bg-secondary/80 rounded-xl border-2 border-solid border-ternary">
+    
     <FormComponentSimple label="title" type="text" classString="flex justify-center"/>
 
     <FormComponentSimple label="date" type="date"/>
@@ -23,14 +24,14 @@
     <FormComponentSimple label="picture" type="file"/>
     
     <label for="categories">Categories:</label>
-    <select id="categories" bind:value={categories} required>
+    <select id="categories" bind:value={categories} required class="text-black rounded-xl border-2 border-solid border-ternary">
         {#each uniqueCategories as cat}
             <option value="{cat}">{cat}</option>
         {/each}
     </select><hr class="border-0 my-2">
 
     <label for="content">Content:</label>
-    <textarea id="content" required></textarea><hr class="border-0 my-2">
+    <textarea id="content" required class="text-black rounded-xl border-2 border-solid border-ternary"></textarea><hr class="border-0 my-2">
 
     <button type="submit">Submit</button>
 </form>
